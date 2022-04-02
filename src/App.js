13b52bfbf8c8a33fd81009afb008b8a7be6e0f3a
Franklyn Urey Guardado <franklyn.urey@kiumas.net>
2022-04-02 01:26:27 -0600

    Category Item Component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,42 @@
-import './categories.styles.scss';
+import CategoryItem from "./components/category-item/category-item.component";
+import "./categories.styles.scss";
 
 const App = () => {
-
   const categories = [
     {
-      id:1,
-      title:'Firenze'
+      id: 1,
+      title: "Firenze",
+      imageUrl: "/assets/Firenze.jpg",
     },
     {
-      id:2,
-      title:'Lakme'
+      id: 2,
+      title: "Lakme",
+      imageUrl: "/assets/Lakme.jpg",
     },
     {
-      id:3,
-      title:'BBCOS'
+      id: 3,
+      title: "BBCOS",
+      imageUrl: "/assets/BBCOS.png",
     },
     {
-      id:4,
-      title:'Sorme'
+      id: 4,
+      title: "Sorme",
+      imageUrl: "/assets/Sorme.jpg",
     },
     {
-      id:5,
-      title:'Headshot'
-    }
-  ]
+      id: 5,
+      title: "Headshot",
+      imageUrl: "/assets/No_image_available.png",
+    },
+  ];
 
   return (
     <div className="categories-container">
-      {categories.map(({id,title}) => (
-          <div key={id} className="category-container">
-          <div className="background-image"></div>
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Compra ahora</p>
-          </div>
-          </div>
-        ))}
-      </div>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
   );
-}
+};
 
 export default App;
