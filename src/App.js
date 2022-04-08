fbd68dff5f31cf2817e22bae71695d8168561604
Franklyn Urey Guardado <franklyn.urey@kiumas.net>
2022-04-08 11:05:50 -0600

    Setting Up Our HomePage

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
-import Directory from "./components/directory/directory.component";
-import './App.css';
-
+import { Routes,Route } from 'react-router-dom';
+import Home from './routes/home/home.component';
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Firenze",
-      imageUrl: "/assets/Firenze.jpg",
-    },
-    {
-      id: 2,
-      title: "Lakme",
-      imageUrl: "/assets/Lakme.jpg",
-    },
-    {
-      id: 3,
-      title: "BBCOS",
-      imageUrl: "/assets/BBCOS.png",
-    },
-    {
-      id: 4,
-      title: "Sorme",
-      imageUrl: "/assets/Sorme.jpg",
-    },
-    {
-      id: 5,
-      title: "Headshot",
-      imageUrl: "/assets/No_image_available.png",
-    },
-  ];
-
-  return <Directory categories={categories} />;
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/home' element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
